5417ac02938602eafb19e23077fec2f772e88e79
Pavel Dolgov <70911716+mamont79@users.noreply.github.com>
2021-03-15 23:57:05 +0300

    add: some terrible solution

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+    let array = [];
+    let str = '';
+    let i = 0;
+    let j = 0;
+    while (i < matrix.length) {
+        while (j < matrix[i].length) {
+            str = str + matrix[i][j];
+            j++;
+        }
+        i++;
+
+    }
+
+
+    return str.split('');
 }
