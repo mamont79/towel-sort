caa093964857865f24b92703d7ad130705ffe4e5
Pavel Dalhou <70911716+mamont79@users.noreply.github.com>
2023-01-16 09:58:39 +0100

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -3,17 +3,16 @@
 module.exports = function towelSort(matrix) {
     let array = [];
     let str = '';
-    let i = 0;
-    let j = 0;
-    while (i < matrix.length) {
-        while (j < matrix[i].length) {
-            str = str + matrix[i][j];
-            j++;
+    if (matrix) {
+        for (let i = 0; i < matrix.length; i++) {
+            for (let j = 0; j < matrix[i].length; j++) {
+                if (i % 2 == 0) {
+                    array.push(matrix[i][j]);
+                } else if (i % 2 == 1) {
+                    array.push(matrix[i][matrix[i].length - j - 1]);
+                }
+            }
         }
-        i++;
-
     }
-
-
-    return str.split('');
+    return array;
 }
